ab069b12999e05eab54b660c0df505ca0112e5cb
bit_guber <mgrguber@gmail.com>
2023-10-19 10:25:25 +0530

    add header response

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
@@ -11,15 +11,15 @@ file_path = "resources/"
 import os
 print( os.listdir() )
 max_display_movies = 10
-movies = pd.read_csv( file_path + "links.csv", dtype = { "tmdbId":"str"} ).set_index( "movieId" ).loc[:, "tmdbId" ].to_dict( )
-reverse_movies = { v: k for k,v in movies.items() }
+movies = pd.read_csv( file_path + "links.csv", dtype = { "tmdbId":"str", "movieId":np.int32 } ).set_index( "movieId" ).loc[:, "tmdbId" ].to_dict( ) # { k:movieId , v:tmdbId }
+reverse_movies = { v: k for k,v in movies.items() } # { k:tmdbId, v:movieId }
 vectors = np.load(  file_path + "movie_vectors.npy" )
 mapping_id = None
 with open( file_path + "movie_id_to_vector_id.json", "r" ) as f:
-    mapping_id = { int(k):int(v) for k,v in json.load( f ).items()}
-top_movies = np.load( file_path + "top_movies.npy" )[:max_display_movies]
+    mapping_id = { int(k):int(v) for k,v in json.load( f ).items()} # { k:movieId, v:fittedMovieId }
+top_movies = np.load( file_path + "top_movies.npy" )[:max_display_movies].tolist() # top 100 movies of movieId
 
-reverse_mapping_id = { v:k for k, v in mapping_id.items() }
+reverse_mapping_id = { v:k for k, v in mapping_id.items() } # { k:fittedMovieId, v:movieId }
 each_movie_distances = dict()
 def correlation( u, v, centered=False):
     if centered:
@@ -49,19 +49,21 @@ def get_cosine_distance( target ):
         each_movie_distances[target] = distances
         return each_movie_distances[target]
 
-def pre_look_up( items ):
+def pre_processing( items ):
     return [reverse_movies[i] for i in items ]
 
-def look_up( items ):
-    return [ movies[i] for i in items ]
+def post_processing( items ):
+    resp = Response( [ movies[i] for i in items ] )
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 @app.route( "/get-list", methods = ['POST'] )
 def get_list( ):
-    viewed = pre_look_up(request.get_json()["viewed"])
+    viewed = pre_processing(request.get_json()["viewed"])
     if viewed is None :
-        return look_up(top_movies.tolist())
+        return post_processing(top_movies)
     if len(viewed) == 0:
-         return top_movies.tolist()
+         return post_processing(top_movies)
     cummalate = np.zeros( vectors.shape[0], dtype = np.float32 )
     viewed = [ mapping_id[x] for x in viewed ]
     start_time = time.time()
@@ -69,7 +71,7 @@ def get_list( ):
         cummalate += get_cosine_distance( target  )
     print( time.time() - start_time )
     viewed = set(viewed)
-    return look_up([reverse_mapping_id[x] for x in np.argsort( cummalate ) if x not in viewed ][:max_display_movies])
+    return post_processing([reverse_mapping_id[x] for x in np.argsort( cummalate ) if x not in viewed ][:max_display_movies])
 
 @app.route("/")
 def hello_world():
